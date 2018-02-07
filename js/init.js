56b0fd64d78e90ec185507ad9cf01378f1029805
Kelvin Man <kelvinman.tk@gmail.com>
2018-02-07 01:08:03 -0800

    initial commit

---
+++ js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+      $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+      });
+      $(".button-collapse").sideNav({
+        closeOnClick: true,
+        edge: 'right'
+      });
+    });
+
+$(function () {
+      $(document).scroll(function () {
+        var $nav = $(".fixed_nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+    });
